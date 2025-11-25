5f2bca03ed8dc56cdb386000c25c09e7b15254ed
zahra-hsb <zahra16toa@gmail.com>
2025-11-25 02:57:07 -0800

    feat: create types file

---
+++ utils/schemas/types.ts
@@ -0,0 +1,16 @@
+import { HTMLInputTypeAttribute, ReactNode } from "react";
+import { FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+export interface TextInputType<TFormValues extends FieldValues> {
+    label?: string;
+    id: string;
+    registerId: Path<TFormValues>;
+    type: HTMLInputTypeAttribute;
+    className?: string;
+    placeholder?: string;
+    dir?: "ltr" | "rtl";
+    icon?: ReactNode;
+    name: string;
+    required: boolean;
+    register: UseFormRegister<TFormValues>;
+}
